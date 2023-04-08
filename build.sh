48492f59276f4e47caf7b7dc621ec247088ca994
Matt Richards <matt@kreative.co.uk>
2023-04-08 14:40:48 +0100

    Tries to solve Poetry version issue on Render.

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
-poetry install
+# poetry install
+echo "Installing the latest version of poetry..."
+pip install --upgrade pip
+pip install poetry==1.4.2
+rm poetry.lock
+poetry lock
+python -m poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
