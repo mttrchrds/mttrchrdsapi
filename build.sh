f6130614a0ca5445fba31eedc5cc347487a9b055
Matt Richards <matt@kreative.co.uk>
2023-04-09 10:56:37 +0100

    Updates createsuperuser command with more params.

--- build.sh
+++ build.sh
@@ -15,5 +15,5 @@ python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
 then
-  python manage.py createsuperuser --no-input
+  python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 fi
