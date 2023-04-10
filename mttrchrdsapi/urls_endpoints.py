7f8fda0485ff89640b3d13116a2f51c0c4ceb3db
Matt Richards <matt@kreative.co.uk>
2023-04-10 15:43:54 +0100

    Adds Swagger API documentation.

--- mttrchrdsapi/urls_endpoints.py
+++ mttrchrdsapi/urls_endpoints.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from mttrchrdsapi import views
 
-urlpatterns_api = [
+urlpatterns_endpoints = [
     path('shows/', views.show_list),
     path('shows/<int:id>', views.show_detail),
 ]
