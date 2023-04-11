6a83c6c296f68a639da55f5650d54b98b42a6b92
Matt Richards <matt@kreative.co.uk>
2023-04-11 23:28:54 +0100

    Reset db.

--- mttrchrdsapi/admin.py
+++ mttrchrdsapi/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import Show
-
-admin.site.register(Show)
--- mttrchrdsapi/serializers.py
+++ mttrchrdsapi/serializers.py
@@ -1,7 +1,1 @@
 from rest_framework import serializers
-from .models import Show
-
-class ShowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Show
-        fields = ['id', 'name']
