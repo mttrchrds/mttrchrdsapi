86b4ec6d2dc297d270f35127fc46d4ec3cb6113a
Matt Richards <me@mttrchrds.com>
2024-03-08 09:41:07 +0000

    Loads initial data from fixtures.

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ python -m poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py loaddata mttrchrdsapi/fixtures/initial_data.json
 
-if [[ $CREATE_SUPERUSER ]];
-then
+if [[ $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 fi
